2a5ea718090720e006459d7800c317775b6bba63
stefanradulovicuns <stefanradulovic@uns.ac.rs>
2021-11-25 16:11:03 +0100

    const

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -17,3 +17,8 @@ export const DEPARTMAN_URL = "http://localhost:8083/departman"
 export const STUDENT_URL = "http://localhost:8083/student"
 
 export const STUDENTI_ZA_DEPARTMAN_URL = "http://localhost:8083.com/studentByDepartman"*/
+
+export const MESTO_URL = 'http://localhost:8080/mesto';
+export const KATEG_OBJ_URL = 'http://localhost:8080/kategObj';
+export const OBJEKAT_URL = 'http://localhost:8080/objekat';
+export const LANAC_URL = 'http://localhost:8080/lanac';
