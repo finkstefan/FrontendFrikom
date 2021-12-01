7fffb154dd8bd9aab6455471bf02d8e9529889de
stefanradulovicuns <stefanradulovic@uns.ac.rs>
2021-12-01 10:18:29 +0100

    header

--- src/app/services/dnevni.service.ts
+++ src/app/services/dnevni.service.ts
@@ -1,21 +1,39 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { DNEVNI_URL } from '../app.constants';
 import { Dnevni } from '../models/dnevni'
+import { User } from '../models/user';
+import { AuthorizationService } from './authorization.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DnevniService {
 
-  constructor(public httpClient: HttpClient) { }
+  private token: String;
+  public user: User = {username: 'peraperic', password: '111'};
+  //private headers: HttpHeaders;
+
+  constructor(public httpClient: HttpClient, public authorization: AuthorizationService) {
+    
+  }
 
   public getAllDnevni(): Observable<any> {
-    
-    return this.httpClient.get(`${DNEVNI_URL}`);
-   
+    let header = new HttpHeaders();
+    // this.authorization.login(this.user).subscribe(data => {
+    //   this.token = data.jwt;
+    //   console.log(data);
+    //   header = header.append('Content-Type', 'application/json');
+    //   header = header.append('Authorization', 'Bearer ' + this.token);
+    //   header = header.append('Access-Control-Allow-Origin', '*');
+    //   console.log(header);
+    // });
+    header = header.append('Authorization', 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJwZXJhcGVyaWMiLCJleHAiOjE2MzgzMzMyODEsImlhdCI6MTYzODI5NzI4MX0.zetvXcVa3LhQJoTpuPMyBilgCypxDfgdy5NXmJSnixk');
+    console.log(header);
+    return this.httpClient.get(`${DNEVNI_URL}`, {headers: header});
   }
+
 
   public addDnevni(dnevni: Dnevni): Observable<any> {
     dnevni.idDnevni = 40;
@@ -29,5 +47,6 @@ export class DnevniService {
   public deleteDnevni(id: number): Observable<any> {
     return this.httpClient.delete(`${DNEVNI_URL}/${id}`);
   }
-  
+
 }
+
